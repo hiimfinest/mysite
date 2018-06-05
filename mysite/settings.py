d3d7c184a610c5a8313fcb65faa2371fd1cd5936
HiimFinest <31751323+HiimFinest@users.noreply.github.com>
2018-06-05 07:20:03 -0700

    updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'PDT'
 
-USE_I18N = True
+USE_I18N = True 
 
 USE_L10N = True
 
